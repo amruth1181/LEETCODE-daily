d7e4df35232f368feff0cf1807e63fbd895ce24b
Ganta Amruth <amruth@Ganta-ka-MacBook-Air.local>
2026-02-01 16:03:40 +0100

    maximum volume of a container using two pointer

---
+++ maxvol2pointer.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        vol = 0
+        i = 0
+        n = len(height)
+        j = n - 1
+        while i < j:
+            curr_vol = min(height[i], height[j]) * (j - i)
+            vol = max(curr_vol, vol)
+            if height[i] > height[j]:
+                j -= 1
+            else:
+                i += 1
+
+        return vol
